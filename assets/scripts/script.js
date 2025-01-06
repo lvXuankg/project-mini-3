a261457740e0027bf5f9d88752cf254ff7686983
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-01-07 00:53:35 +0700

    finish header and create banner 1

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,3 +3,10 @@ const input = document.getElementById("search");
 input.placeholder = "\uf002 " + input.placeholder; 
 input.style.fontFamily = "Font Awesome 5 Free"; 
 input.style.fontWeight = "400"; 
+
+//Banner 1
+const buttonClose1 = document.querySelector("#closeBanner1");
+buttonClose1.addEventListener("click", ()=>{
+    const divBanner1 = document.querySelector("#banner-1");
+    divBanner1.style.display = "none";
+});
