afdf69810cdee2efcee6f0e058a77e73de7d4b68
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-01-06 23:43:40 +0700

    create slogan + header

---
+++ assets/scripts/script.js
@@ -0,0 +1,5 @@
+// Thêm icon vào thanh tìm kiếm 
+const input = document.getElementById("search");
+input.placeholder = "\uf002 " + input.placeholder; 
+input.style.fontFamily = "Font Awesome 5 Free"; 
+input.style.fontWeight = "400"; 
