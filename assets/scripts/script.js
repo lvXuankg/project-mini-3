27025523162bf1d8f271e3ec25d348e85b9dca99
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-01-07 15:27:13 +0700

    Thêm database, phần bán hàng và hiển thị hàng

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,13 @@
+// FetchApi 
+const fetchApi = (url)=>{
+    var result = fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        });
+    return result;
+}
+
 // Thêm icon vào thanh tìm kiếm 
 const input = document.getElementById("search");
 input.placeholder = "\uf002 " + input.placeholder; 
@@ -10,3 +20,53 @@ buttonClose1.addEventListener("click", ()=>{
     const divBanner1 = document.querySelector("#banner-1");
     divBanner1.style.display = "none";
 });
+
+//Sell Gocery
+const divSell = document.querySelector("#sellGocery");
+var urls = [
+    "http://localhost:3000/humans",
+    "http://localhost:3000/goods",
+    "http://localhost:3000/laptop",
+    "http://localhost:3000/books",
+    "http://localhost:3000/telephone",
+    "http://localhost:3000/mita"
+]
+
+async function getAllApi(){
+    var dataApi = [];
+    for(x of urls){
+        let data = await fetchApi(x);
+        dataApi = await dataApi.concat(data);
+    }
+    return dataApi;
+
+}
+getAllApi()
+    .then(data => {
+        let htmls = `
+        <div class="container">
+        <div class="box">
+        <div class="row">
+            `;
+        for(x of data){
+            htmls += `
+                <div class="col-xl-2 col-lg-3 col-md-6 col-6">
+                    <div class="inner-wrap">
+                        <div class="imageContainer">
+                            <img src="${x.image}">
+                        </div>
+                        <h3>${x.name}</h3>
+                        <p>Giá : ${x.price}</p>
+                        <p>Còn lại: ${x.slots}</p>
+                        <button>Thêm vào giỏ hàng</button>
+                    </div>
+                </div>
+            `;
+        }
+        htmls += `
+        </div>
+        </div>
+        </div>
+        `;
+        divSell.innerHTML = htmls;
+    })
